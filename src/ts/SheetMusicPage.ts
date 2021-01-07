72218989850797bc87487a867d8722dac34c2899
Vasily Kovnev <v.k95@yandex.ru>
2021-01-08 00:23:30 +0300

    feat: displaying Legato and Staccato. Feat: note sections are wrapping to next line when screen shrinks

--- src/ts/SheetMusicPage.ts
+++ src/ts/SheetMusicPage.ts
@@ -1,44 +1,89 @@
 import Vex from 'vexflow';
 import { track } from '../../public/songs/Intertainer';
-import { NoteName} from '../models/Notations';
+import { NoteName, Touch, Clef } from '../models/Notations';
+
+const SECTION_SIZE = {
+  width: 400,
+  height: 150,
+};
 
 export default class SheetMusicPage {
-  parentElement: HTMLElement;
+  parentElement: HTMLDivElement;
+  renderElement: HTMLDivElement;
   VF: any;
   context: any;
+  sectionSize: { width: number; height: number };
 
-  constructor(parentElement: HTMLElement) {
+  constructor(parentElement: HTMLDivElement) {
     this.parentElement = parentElement;
     this.VF = Vex.Flow;
     this.context = null;
+    this.renderElement = null;
+  }
+
+  getTies(notesList) {
+    let tiesList = [];
+    //Нужно подумать как сделать последовательность если связь будет у нескольких нот подряд
+    for (let i = 0; i < notesList.ties.length; i = i + 2) {
+      const tieObj = new this.VF.StaveTie({
+        first_note: notesList.notes[notesList.ties[i]],
+        last_note: notesList.notes[notesList.ties[i + 1]],
+      });
+
+      tiesList.push(tieObj);
+    }
+
+    return tiesList;
   }
 
   drawStaveMeasure(section) {
     const timeSignature = `${section.Size.Count}/${section.Size.Per}`;
-    const sectionSizeX = 300;
 
-    section.Measures.forEach((measure, index) => {
-      const stave = new this.VF.Stave(sectionSizeX * index, 0, sectionSizeX);
+    section.Measures.forEach((measure, index: number) => {
+      const renderer = new this.VF.Renderer(this.renderElement, this.VF.Renderer.Backends.SVG);
 
-      if (index === 0) stave.addClef('treble').addTimeSignature(timeSignature);
+      // Size our SVG:
+      renderer.resize(SECTION_SIZE.width, SECTION_SIZE.height);
+
+      // And get a drawing context:
+      this.context = renderer.getContext();
+      this.context.setViewBox(
+        SECTION_SIZE.width * index,
+        0,
+        SECTION_SIZE.width,
+        SECTION_SIZE.height,
+      );
+
+      const stave = new this.VF.Stave(SECTION_SIZE.width * index, 0, SECTION_SIZE.width);
+
+      if (index === 0) stave.addClef(Clef[section.Clef]).addTimeSignature(timeSignature);
 
       stave.setContext(this.context).draw();
 
-      const notesArray = this.getNotesArray(measure.Notes);
-      const beams = this.VF.Beam.generateBeams(notesArray);
+      const notesList = this.getNotesArray(measure.Notes);
+      const beams = this.VF.Beam.generateBeams(notesList.notes);
 
-      this.VF.Formatter.FormatAndDraw(this.context, stave, notesArray);
+      this.VF.Formatter.FormatAndDraw(this.context, stave, notesList.notes);
 
       beams.forEach((beam) => {
         beam.setContext(this.context).draw();
+      });
+
+      const ties = this.getTies(notesList);
+
+      ties.forEach((tie) => {
+        tie.setContext(this.context).draw();
       });
     });
   }
 
   getNotesArray(notes) {
-    let notesArray = [];
+    const notesList = {
+      notes: <any>[],
+      ties: <any>[],
+    };
 
-    notes.forEach((notes) => {
+    notes.forEach((notes, index: number) => {
       let note = notes.Notes[0];
 
       if (typeof note.Name === 'undefined') return;
@@ -51,25 +96,39 @@ export default class SheetMusicPage {
       });
 
       //noteObj.addAccidental(0, new this.VF.Accidental("b"));
-      //добавить лигу (связка ноты)
       if (note.IsDotted) noteObj.addDot(0);
 
-      notesArray.push(noteObj);
+      notesList.notes.push(noteObj);
+
+      if (note.Touch === Touch.Legato) {
+        notesList.ties.push(index);
+        return;
+      }
+
+      if (note.Touch === Touch.Staccato) {
+        noteObj.addModifier(0, new this.VF.Dot());
+        return;
+      }
     });
 
-    return notesArray;
+    return notesList;
+  }
+
+  addBitrate(renderElement: HTMLDivElement) {
+    const bitrateContainer = document.createElement('div');
+    bitrateContainer.classList.add('sheet-music__bitrate');
+    bitrateContainer.textContent = `Bpm = ${track.Sections[0].Bpm}`;
+    renderElement.append(bitrateContainer);
   }
 
   render() {
     console.log(track);
 
-    const renderer = new this.VF.Renderer(this.parentElement, this.VF.Renderer.Backends.SVG);
+    this.addBitrate(this.parentElement);
 
-    // Size our SVG:
-    renderer.resize(1200, 600);
-
-    // And get a drawing context:
-    this.context = renderer.getContext();
+    this.renderElement = document.createElement('div');
+    this.renderElement.classList.add('sheet-music__render');
+    this.parentElement.append(this.renderElement);
 
     track.Sections.forEach((section) => {
       this.drawStaveMeasure(section);
