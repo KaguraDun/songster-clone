febded4482ae066c863c144eccc66dd0089000ea
Vasily Kovnev <v.k95@yandex.ru>
2021-01-11 01:19:08 +0300

    feat: add timeMarker;
    Time marker moves throught measures correctly.

--- src/ts/SheetMusicPage.ts
+++ src/ts/SheetMusicPage.ts
@@ -1,5 +1,5 @@
 import Vex from 'vexflow';
-import * as track from '../../public/songs/4.json';
+import * as track from '../../public/songs/2.json';
 import { Touch, Alteration } from '../models/Notations';
 import { Chord, Measure, Note, NoteTie, Track } from '../models/TrackDisplayType';
 
@@ -11,10 +11,18 @@ const SECTION_SIZE = {
 export default class SheetMusicPage {
   parentElement: HTMLDivElement;
   renderElement: HTMLDivElement;
+  timeMarker: HTMLDivElement;
+  timeMarkerTimer: any; // dfdf
+  playMusic: boolean;
 
   constructor(parentElement: HTMLDivElement) {
     this.parentElement = parentElement;
     this.renderElement = null;
+    this.timeMarker = null;
+    this.timeMarkerTimer = null;
+    this.playMusic = false;
+    this.playMusicTrack = this.playMusicTrack.bind(this);
+    this.changeTimeMarkerPosition = this.changeTimeMarkerPosition.bind(this);
   }
 
   getTies(notesList: NoteTie) {
@@ -35,6 +43,9 @@ export default class SheetMusicPage {
 
   drawStaveMeasures(measures: Measure[]) {
     const timeSignature = `${track.Size.Count}/${track.Size.Per}`;
+    const quarter = 60 / track.Bpm;
+    const measureDuration = (4 * quarter * track.Size.Count) / track.Size.Per;
+    const timeMarkerSpeed = SECTION_SIZE.width / measureDuration;
 
     measures.forEach((measure, index: number) => {
       const context = Vex.Flow.Renderer.getSVGContext(
@@ -42,12 +53,7 @@ export default class SheetMusicPage {
         Vex.Flow.Renderer.Backends.SVG,
       );
 
-      context.setViewBox(
-        SECTION_SIZE.width * index,
-        0,
-        SECTION_SIZE.width,
-        SECTION_SIZE.height,
-      );
+      context.setViewBox(SECTION_SIZE.width * index, 0, SECTION_SIZE.width, SECTION_SIZE.height);
 
       const stave = new Vex.Flow.Stave(SECTION_SIZE.width * index, 0, SECTION_SIZE.width);
 
@@ -69,6 +75,10 @@ export default class SheetMusicPage {
       ties.forEach((tie) => {
         tie.setContext(context).draw();
       });
+
+      const svgElement: HTMLElement = this.renderElement.children[index];
+      svgElement.dataset.time = measure.Time;
+      svgElement.dataset.markerSpeed = timeMarkerSpeed;
     });
   }
 
@@ -111,20 +121,87 @@ export default class SheetMusicPage {
     return { notes, ties };
   }
 
-  addBitrate(renderElement: HTMLDivElement) {
+  addBitrate(parentElement: HTMLDivElement) {
     const bitrateContainer = document.createElement('div');
     bitrateContainer.classList.add('sheet-music__bitrate');
     bitrateContainer.textContent = `Bpm = ${Math.trunc(track.Bpm)}`;
-    renderElement.append(bitrateContainer);
+    parentElement.append(bitrateContainer);
+  }
+
+  addButtonPlay(parentElement: HTMLDivElement): HTMLButtonElement {
+    const buttonContainer = document.createElement('button');
+    buttonContainer.classList.add('button-play');
+    buttonContainer.textContent = `play`;
+    parentElement.append(buttonContainer);
+    return buttonContainer;
+  }
+
+  moveTimeMarker(timeMarker: HTMLDivElement) {
+    const shiftOffset: number = 1;
+    const firstMeasure = this.renderElement.children[1];
+    const numberElementPerRow = Math.floor(document.body.clientWidth / firstMeasure.clientWidth);
+    const firstRowStartPosition = firstMeasure.getBoundingClientRect();
+    const firstRowEndPosition = firstRowStartPosition.x + numberElementPerRow * SECTION_SIZE.width;
+
+    if (timeMarker.offsetLeft > firstRowEndPosition) {
+      timeMarker.style.left = `${firstRowStartPosition.x}px`;
+      timeMarker.style.top = `${timeMarker.offsetTop + SECTION_SIZE.height}px`;
+      timeMarker.scrollIntoView();
+      return;
+    }
+
+    timeMarker.style.left = `${timeMarker.offsetLeft + shiftOffset}px`;
+  }
+
+  playMusicTrack(event) {
+    this.playMusic = !this.playMusic;
+
+    if (this.playMusic) {
+      event.target.textContent = 'stop';
+      this.timeMarkerTimer = setInterval(() => this.moveTimeMarker(this.timeMarker), 1);
+    } else {
+      event.target.textContent = 'play';
+      clearInterval(this.timeMarkerTimer);
+    }
+  }
+
+  addTimeMarker(parentElement: HTMLDivElement): HTMLDivElement {
+    const timeMarker = document.createElement('div');
+    const timeMarkerStartOffset: number = 100;
+    const firstElementPosition = parentElement.children[0].getBoundingClientRect();
+
+    timeMarker.classList.add('sheet-music__time-marker');
+    timeMarker.style.height = `${SECTION_SIZE.height}px`;
+    timeMarker.style.left = `${firstElementPosition.x + timeMarkerStartOffset}px`;
+
+    parentElement.prepend(timeMarker);
+
+    return timeMarker;
+  }
+
+  changeTimeMarkerPosition(event) {
+    if (event.target.tagName !== 'svg') return;
+    console.log(event.clientY);
+    this.timeMarker.style.left = `${event.clientX}px`;
+
   }
 
   render() {
+    console.log(track.Measures);
     this.addBitrate(this.parentElement);
+
+    // for testing!
+    const buttonPlay = this.addButtonPlay(this.parentElement);
+    buttonPlay.addEventListener('click', this.playMusicTrack);
 
     this.renderElement = document.createElement('div');
     this.renderElement.classList.add('sheet-music__render');
     this.parentElement.append(this.renderElement);
 
     this.drawStaveMeasures(track.Measures);
+
+    this.timeMarker = this.addTimeMarker(this.renderElement);
+
+    this.renderElement.addEventListener('click', this.changeTimeMarkerPosition);
   }
 }
