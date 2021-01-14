263c264cfc98f4dbac6d95cb256439f2796c18f2
julie_meme <julie_meme@mail.ru>
2021-01-14 20:50:32 +0400

    add: main tab container

--- src/ts/DisplayTab.ts
+++ src/ts/DisplayTab.ts
@@ -1,16 +1,39 @@
-export default class DisplayTab{
-    parentElement: HTMLElement;
+export default class DisplayTab {
+  parentElement: HTMLElement;
+  displayContent: HTMLElement;
 
-    constructor (parentElement : HTMLElement){
-        this.parentElement = parentElement;
+  constructor(parentElement: HTMLElement) {
+    this.parentElement = parentElement;
+  }
 
-    }
+  render() {
+    this.displayContent = document.createElement('section');
+    this.displayContent.className = 'display__tab';
+    this.displayContent.setAttribute('id', 'printable');
+    this.parentElement.appendChild(this.displayContent);
+    this.createTabTitle();
+    const tabContent = document.createElement('div');
+    tabContent.className = 'tab__content';
+    this.displayContent.appendChild(tabContent);
+  }
 
-    render(){
-        const displayContent = document.createElement('section');
-        displayContent.className = "display__tab";
-        displayContent.setAttribute('id', 'printable');
-        displayContent.textContent = 'THERE WILL BE TABS';
-        this.parentElement.appendChild(displayContent);
-    }
+  createTabTitle() {
+    const titleComponents = document.createElement('div');
+    titleComponents.className = 'title';
+    const artistName = document.createElement('div');
+    artistName.className = 'title__tab-artist';
+    artistName.textContent = 'NIRVANA';
+    const trackTitle = document.createElement('div');
+    trackTitle.className = 'title__tab-track';
+    trackTitle.textContent = 'Smells like Teen Spirit';
+    const iconBox = document.createElement('div');
+    iconBox.className = 'title__tab-icons';
+    const instrumentIcon = document.createElement('div');
+    instrumentIcon.className = 'title__tab-instrument';
+    const favButton = document.createElement('button');
+    favButton.className = 'title__tab-fav';
+    iconBox.append(instrumentIcon, favButton);
+    titleComponents.append(artistName, trackTitle, iconBox);
+    this.displayContent.appendChild(titleComponents); // коробка загловка
+  }
 }
--- src/ts/Sidebar.ts
+++ src/ts/Sidebar.ts
@@ -15,8 +15,7 @@ export default class Sidebar {
     this.createSpeedButton();
     this.createSoundButton();
     this.createMetronomButton();
-    let button = this.createPrintButton();
-
+    this.createPrintButton();
     this.createFavoriteButton();
     this.createInstrumentButton();
     this.createGitHubButton();
@@ -25,65 +24,49 @@ export default class Sidebar {
   createPlayButton() {
     const playButton = document.createElement('button');
     playButton.className = 'sidebar__button-play';
-    // playButton.textContent = 'play';
     this.sideBarContent.appendChild(playButton);
   }
 
   createSpeedButton() {
     const speedButton = document.createElement('button');
     speedButton.className = 'sidebar__button-speed';
-    // speedButton.textContent='speed';
     this.sideBarContent.appendChild(speedButton);
   }
 
   createSoundButton() {
     const soundButton = document.createElement('button');
     soundButton.className = 'sidebar__button-sound';
-    // soundButton.textContent= 'sound';
     this.sideBarContent.appendChild(soundButton);
   }
   createMetronomButton() {
     const metronomButton = document.createElement('button');
     metronomButton.className = 'sidebar__button-metronom';
-    // metronomButton.textContent = 'metro';
     this.sideBarContent.appendChild(metronomButton);
   }
   createPrintButton() {
     const printButton = document.createElement('button');
     printButton.className = 'sidebar__button-print';
-    // printButton.textContent = 'print';
     this.sideBarContent.appendChild(printButton);
-    // printButton.addEventListener('click', this.printTab());
+    
   }
 
   createFavoriteButton() {
     const favoriteButton = document.createElement('button');
     favoriteButton.className = 'sidebar__button-favorite';
-    // favoriteButton.textContent='fav';
     this.sideBarContent.appendChild(favoriteButton);
   }
 
   createInstrumentButton() {
     const instrumentButton = document.createElement('button');
     instrumentButton.className = 'sidebar__button-instrument';
-    // instrumentButton.textContent = 'inst';
     this.sideBarContent.appendChild(instrumentButton);
   }
 
   createGitHubButton() {
     const gitHubButton = document.createElement('button');
     gitHubButton.className = 'sidebar__button-gitHub';
-    // gitHubButton.textContent = 'git';
     this.sideBarContent.appendChild(gitHubButton);
   }
 
-  printTab(divName: DisplayTab) {
-    let printContent = document.getElementById('printable').innerHTML;
-    let originalContent = document.body.innerHTML;
-
-    document.body.innerHTML = printContent;
-    window.print();
-
-    document.body.innerHTML = originalContent;
-  }
+ 
 }
