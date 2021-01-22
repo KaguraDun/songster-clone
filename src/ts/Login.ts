12b32d6d8e47f6c3f4dbcfdd7cf649ba9dc19c1f
Vasily Kovnev <v.k95@yandex.ru>
2021-01-22 21:06:27 +0300

    fix: now log in and sing up buttons work correctly;
    refactor: move large parts of code into separate functions;

--- src/ts/Login.ts
+++ src/ts/Login.ts
@@ -1,11 +1,8 @@
-
-import { SVG_SPRITE } from "./helpers/svg_sprites";
+import { SVG_SPRITE } from './helpers/svg_sprites';
 import renderElement from './helpers/renderElements';
-import { SingleEntryPlugin } from "webpack";
 
 const LOGGED_IN = 'loggedIn';
 const SHOW = '--show';
-
 
 export default class Login {
   parentElement: HTMLElement;
@@ -15,9 +12,10 @@ export default class Login {
   constructor(parentElement: HTMLElement) {
     this.parentElement = parentElement;
     this.renderFormLogin = this.renderFormLogin.bind(this);
-    this.renderFormSingUp = this.renderFormSingUp.bind(this);
     this.logOut = this.logOut.bind(this);
     this.hideForm = this.hideForm.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
     this.formContainer;
     this.formOverlay;
   }
@@ -25,135 +23,124 @@ export default class Login {
   renderInput(parentElement: HTMLElement, name: string, labelText: string, type?: string) {
     const label = renderElement(parentElement, 'label', [], labelText) as HTMLLabelElement;
     label.htmlFor = name;
+
     const input = renderElement(parentElement, 'input', []) as HTMLInputElement;
     input.name = name;
     input.type = type;
     input.required = true;
+
     return input;
   }
 
-  renderForm(name: string) {
+  async sendRequest(url: string, email: string, password: string, e?: Event) {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    return data;
+  }
+
+  renderForm() {
     this.formContainer.innerHTML = '';
 
     const form = renderElement(this.formContainer, 'form', []) as HTMLFormElement;
     form.action = '/signup';
 
-    // const heading = renderElement(form, 'h2', [], name);
-
-    // const email = this.renderInput(form, 'email', 'Email', 'text');
-    const email = this.renderInput(form, 'email', 'Login', 'text');
+    const email = this.renderInput(form, 'email', 'Email', 'text');
     email.setAttribute('value', 'Enter email...');
-    email.innerText='Enter email...';
+    email.innerText = 'Enter email...';
 
-email.addEventListener('focus', ()=>{
-  email.removeAttribute('value');
-})
+    email.addEventListener('focus', () => {
+      email.removeAttribute('value');
+    });
+
     const emailError = renderElement(form, 'div', ['form_email-error']);
 
     const password = this.renderInput(form, 'password', 'Password', 'password');
     password.setAttribute('value', 'Enter...');
 
-    password.addEventListener('focus', ()=>{
-       password.removeAttribute('value');
-         })
+    password.addEventListener('focus', () => {
+      password.removeAttribute('value');
+    });
 
     const passwordError = renderElement(form, 'div', ['form_password-error']);
 
-    const buttonLogin = renderElement(form, 'button', [], name);
-    const buttonSignup = renderElement(form, 'button', [], "Sign up");
-    buttonLogin.className = 'button_login';
-    buttonSignup.className = 'button_signup';
+    return {
+      form,
+      email,
+      emailError,
+      password,
+      passwordError,
+    };
+  }
 
+  async handleLogin(form: any) {
+    form.emailError.textContent = '';
+    form.passwordError.textContent = '';
 
-    const logSocialBox = renderElement(form, 'div', ['wrapper-user-social']);
-    
-    const ggButton= renderElement(logSocialBox, 'button', ['fb-button']);
-    const fbButton  = renderElement(logSocialBox, 'button', ['gg-button']);
-    fbButton.innerHTML = SVG_SPRITE.FACEBOOK;
-    ggButton.innerHTML = SVG_SPRITE.GOOGLE;
+    const data = await this.sendRequest(
+      'http://localhost:3000/login',
+      form.email.value,
+      form.password.value,
+    );
 
-    return { form, email, emailError, password, passwordError, buttonLogin, buttonSignup, logSocialBox };
+    if (data.errors) {
+      form.emailError.textContent = data.errors.email;
+      form.passwordError.textContent = data.errors.password;
+      return;
+    }
+
+    if (data.user) {
+      localStorage.setItem(LOGGED_IN, 'true');
+      localStorage.setItem('user', `${data.user}`);
+      location.assign('/');
+    }
+  }
+
+  async handleSignIn(form: any) {
+    form.emailError.textContent = '';
+    form.passwordError.textContent = '';
+
+    const data = await this.sendRequest(
+      'http://localhost:3000/signup',
+      form.email.value,
+      form.password.value,
+    );
+
+    if (data.errors) {
+      form.emailError.textContent = data.errors.email;
+      form.passwordError.textContent = data.errors.password;
+      return;
+    }
+
+    if (data.user) {
+      location.assign('/');
+    }
   }
 
   renderFormLogin() {
-    
     this.formOverlay.classList.add(SHOW);
     this.formContainer.classList.add(SHOW);
 
-    const form = this.renderForm('Log in');
+    const form = this.renderForm();
 
-    form.form.addEventListener('submit', async (e) => {
-      e.preventDefault();
+    const buttonLogin = renderElement(this.formContainer, 'button', ['button-login'], 'Log in');
+    const buttonSignup = renderElement(this.formContainer, 'button', ['button-signup'], 'Sign up');
+    const logSocialBox = renderElement(this.formContainer, 'div', ['social']);
 
-      // reset errors
-      form.emailError.textContent = '';
-      form.passwordError.textContent = '';
+    const ggButton = renderElement(logSocialBox, 'button', ['gg-button']);
+    const fbButton = renderElement(logSocialBox, 'button', ['fb-button']);
+    fbButton.innerHTML = SVG_SPRITE.FACEBOOK;
+    ggButton.innerHTML = SVG_SPRITE.GOOGLE;
 
-      // get values
-      const email = form.email.value;
-      const password = form.password.value;
-
-      const res = await fetch('http://localhost:3000/login/', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      const data = await res.json();
-
-      if (data.errors) {
-        form.emailError.textContent = data.errors.email;
-        form.passwordError.textContent = data.errors.password;
-        return;
-      }
-
-      if (data.user) {
-        localStorage.setItem(LOGGED_IN, 'true');
-        localStorage.setItem('user', `${data.user}`);
-        location.assign('/');
-      }
-    });
-  }
-
-  renderFormSingUp() {
-    
-    this.formOverlay.classList.add(SHOW);
-    this.formContainer.classList.add(SHOW);
-
-    const form = this.renderForm('Sign up');
-
-    form.form.addEventListener('submit', async (e) => {
-      e.preventDefault();
-
-      // reset errors
-      form.emailError.textContent = '';
-      form.passwordError.textContent = '';
-
-      // get values
-      const email = form.email.value;
-      const password = form.password.value;
-
-      const res = await fetch('http://localhost:3000/', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      const data = await res.json();
-
-      console.log(data);
-
-      if (data.errors) {
-        form.emailError.textContent = data.errors.email;
-        form.passwordError.textContent = data.errors.password;
-        return;
-      }
-
-      if (data.user) {
-        location.assign('/');
-      }
-    });
+    buttonLogin.addEventListener('click', () => this.handleLogin(form));
+    buttonSignup.addEventListener('click', () => this.handleSignIn(form));
   }
 
   async logOut() {
@@ -173,39 +160,22 @@ email.addEventListener('focus', ()=>{
   }
 
   render() {
-
-    // const loginButton = document.createElement('button');
-    // loginButton.className = 'wrapper-user-login';
-    // loginButton.innerHTML = SVG_SPRITE.LOGIN;
-    // this.parentElement.appendChild(loginButton);
-
     this.formContainer = renderElement(this.parentElement, 'div', ['wrapper-user__form-container']);
     this.formOverlay = renderElement(this.parentElement, 'div', ['wrapper-user__form-overlay']);
 
     this.formOverlay.addEventListener('click', this.hideForm);
 
     if (localStorage.getItem(LOGGED_IN)) {
-      const buttonLogOut = renderElement(this.parentElement, 'button', ['wrapper-user-log-out']);
+      const buttonLogOut = renderElement(this.parentElement, 'button', ['button-log-out']);
       buttonLogOut.title = 'Log Out';
       buttonLogOut.addEventListener('click', this.logOut);
       return;
     }
 
-    const buttonLogin = renderElement(this.parentElement, 'button', ['wrapper-user-login']);
+    const buttonLogin = renderElement(this.parentElement, 'button', ['button-user-login']);
     buttonLogin.addEventListener('click', this.renderFormLogin);
     buttonLogin.title = 'Log in';
 
-    // const buttonSingUp = renderElement(this.parentElement, 'button', ['wrapper-user-signup']);
-    // buttonSingUp.addEventListener('click', this.renderFormSingUp);
-    // buttonSingUp.title = 'Sing up';
-
-    buttonLogin.innerHTML= SVG_SPRITE.LOGIN;
-    // buttonSingUp.innerHTML = SVG_SPRITE.SIGNIN;
-
-   
-  
-
+    buttonLogin.innerHTML = SVG_SPRITE.LOGIN;
   }
-
- 
 }
