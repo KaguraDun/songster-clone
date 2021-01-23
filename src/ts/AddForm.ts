a9c95813b636833383d43bcb8574f7a301eb5093
julie_meme <julie_meme@mail.ru>
2021-01-24 01:18:50 +0400

    add: drag n drop form

--- src/ts/AddForm.ts
+++ src/ts/AddForm.ts
@@ -1,6 +1,5 @@
 import { Difficulty } from '../models/Difficulty';
 import { Genre } from '../models/Genre';
-import { InstrumentType } from '../models/Instrument';
 import renderElement from './helpers/renderElements';
 import { SVG_SPRITE } from './helpers/svg_sprites';
 
@@ -13,15 +12,21 @@ export default class AddForm {
   instrumentInput: HTMLSelectElement;
   genreInput: HTMLSelectElement;
   difficultyInput: HTMLSelectElement;
+  dropArea: HTMLElement;
+  text: HTMLElement;
+  fileName: string;
 
   constructor(parentElement: HTMLElement) {
     this.parentElement = parentElement;
     this.renderForm = this.renderForm.bind(this);
     this.hideForm = this.hideForm.bind(this);
-
+    this.activeArea = this.activeArea.bind(this);
     this.formContainer;
     this.formOverlay;
     this.formContainer;
+    this.dropArea;
+    this.text;
+    this.fileName;
   }
 
   renderInput(parentElement: HTMLElement, name: string, labelText: string, type?: string) {
@@ -69,7 +74,7 @@ export default class AddForm {
 
     const container = renderElement(form, 'div', ['options-container']);
 
-    this.instrumentInput = this.renderOptionSelectAndGet(InstrumentType, 'Instrument', container);
+    // this.instrumentInput = this.renderOptionSelectAndGet(InstrumentType, 'Instrument', container);
     this.genreInput = this.renderOptionSelectAndGet(Genre, 'Genre', container);
     this.difficultyInput = this.renderOptionSelectAndGet(Difficulty, 'Difficulty', container);
 
@@ -85,14 +90,13 @@ export default class AddForm {
     const authorError = renderElement(form, 'div', ['form-add__name-error']);
     authorError.textContent = '';
 
-  
+    this.renderUploadForm(form);
+
     const buttonContainer = renderElement(form, 'div', ['form-add__button-container']);
     const buttonSubmit = renderElement(buttonContainer, 'button', ['button-submit'], 'Submit');
 
     const buttonCancel = renderElement(buttonContainer, 'button', ['button-cancel'], 'Cancel');
     buttonCancel.addEventListener('click', this.hideForm);
-
-   
   }
 
   renderOptionSelectAndGet(e: any, name: string, parentElement: HTMLElement) {
@@ -112,10 +116,71 @@ export default class AddForm {
     parentElement.appendChild(select);
     return select;
   }
-
-
   hideForm() {
     this.formOverlay.classList.remove(SHOW);
     this.formContainer.classList.remove(SHOW);
+  }
+
+  renderUploadForm(parentElement: HTMLElement) {
+    this.dropArea = renderElement(parentElement, 'div', ['drop-area']);
+    this.text = renderElement(this.dropArea, 'div', [], 'Drop your file here');
+    this.dropArea.innerHTML = SVG_SPRITE.UPLOAD;
+
+    const file = this.renderInput(this.dropArea, 'input', 'Upload the file', 'file');
+    file.classList.add('drop-area__input');
+
+    document.querySelectorAll('.drop-area__input').forEach((inputElement) => {
+      const dropAreaElement = inputElement.closest('.drop-area');
+
+      dropAreaElement.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        dropAreaElement.classList.add('drop-area__over');
+      });
+
+      ['dragleave', 'dragend'].forEach((type) => {
+        dropAreaElement.addEventListener(type, (e) => {
+          dropAreaElement.classList.remove('drop-area__over');
+        });
+      });
+
+      dropAreaElement.addEventListener('drop', (e: DragEvent) => {
+        this.dropHandler(e);
+        this.changeDropAreaView();
+      });
+    });
+  }
+
+  changeDropAreaView() {
+    this.dropArea.innerHTML = SVG_SPRITE.DONE;
+    this.text = renderElement(this.dropArea, 'div', [], this.fileName);
+  }
+
+  dropHandler(ev: DragEvent) {
+    console.log('File(s) dropped');
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          this.fileName = `${file.name}`;
+          // console.log('... file[' + i + '].name = ' + file.name);
+          console.log(this.fileName);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        this.fileName = `${ev.dataTransfer.files[i].name}`;
+        // console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+        console.log(this.fileName);
+      }
+    }
+  }
+
+  activeArea() {
+    this.dropArea.classList.add('.drop-area__over');
   }
 }
