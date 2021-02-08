7c902326a2b186c773636054db6655c0207153c4
julie_meme <julie_meme@mail.ru>
2021-02-09 02:11:56 +0400

    add: preloader

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,34 @@
 import '../styles/index.scss';
 import Store, { EVENTS } from './Store';
 import Page from './Page';
+import renderElement from './helpers/renderElements';
 
 const store = new Store();
 store.init();
+
+
+const preloader = document.createElement('div');
+preloader.classList.add('preloader');
+const preloaderRow = renderElement(preloader, 'div', ['preloader__row']);
+const preloaderItem1 = renderElement(preloaderRow, 'div', ['preloader__item']);
+const preloaderItem2 = renderElement(preloaderRow, 'div', ['preloader__item']);
 
 const container = document.createElement('div');
 container.classList.add('wrapper');
 document.body.appendChild(container);
 new Page(container,store).render();
+document.body.appendChild(preloader);
+
+window.onload = function () {
+    window.setTimeout(function() {
+    document.body.classList.add('loaded_hiding');
+}, 300);
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
+
+
+
+
